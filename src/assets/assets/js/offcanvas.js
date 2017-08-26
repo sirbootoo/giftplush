8d9cd455bf802575359b8c047a4a26d2631ad01e
walez18 <madarasage@gmail.com>
2017-08-26 16:03:09 +0100

    designs

---
+++ src/assets/assets/js/offcanvas.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function () {
+
+
+  jQuery('[data-toggle="offcanvas"]').on('click', function () {
+    jQuery('.row-offcanvas').toggleClass('active');
+  }); 
+
+
+
+});
+
+
+
+document.querySelector('body').addEventListener('click', function(){
+  if(event.target.className === 'row-offcanvas'){
+    console.log('Toggle Should Work');
+  }
+});
+
+
